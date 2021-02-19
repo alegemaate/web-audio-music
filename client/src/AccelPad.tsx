af92428538fbc56906d2e2a77d08cf1af3762e4c
Allan Legemaate <alegemaate@gmail.com>
2021-02-19 15:42:06 -0500

    Fix sound on ios

---
+++ client/src/AccelPad.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { colorMap, rangeMap } from "./helpers";
+import GyroNorm from "gyronorm";
+import { Box, Button, Card, CardContent, Typography } from "@material-ui/core";
+import { Error } from "@material-ui/icons";
+
+const DOT_SIZE = 100;
+
+export type AccelParams = {
+  alpha: number;
+  beta: number;
+  gamma: number;
+};
+
+export const AccelPad: React.FC<{
+  onChange: (args: AccelParams) => void;
+}> = ({ onChange }) => {
+  const [hasMotionDevice, setHasMotionDevice] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [size, setSize] = React.useState({ width: 0, height: 0 });
+
+  const boardRef = React.useRef<HTMLDivElement>(null);
+  const dotRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (boardRef.current) {
+      setSize({
+        width: boardRef.current.clientWidth,
+        height: boardRef.current.clientHeight,
+      });
+    }
+  }, [boardRef]);
+
+  const motionAccess = () => {
+    const gn = new GyroNorm();
+
+    if (!window.DeviceMotionEvent?.requestPermission) {
+      setError("This browser does not support device motion");
+    }
+
+    window.DeviceMotionEvent?.requestPermission?.().then((response) => {
+      if (response === "granted") {
+        setHasMotionDevice(true);
+
+        gn.init({ frequency: 50 })
+          .then(() => {
+            gn.start((data) => {
+              if (dotRef.current) {
+                const { alpha, beta, gamma } = data.do;
+
+                dotRef.current.style.left = `${
+                  rangeMap(gamma, -90, 90, 0, size.width) - DOT_SIZE / 2
+                }px`;
+
+                dotRef.current.style.top = `${
+                  rangeMap(beta, -90, 90, 0, size.height) - DOT_SIZE / 2
+                }px`;
+
+                dotRef.current.style.backgroundColor = `rgb(255,${colorMap(
+                  beta,
+                  -180,
+                  180
+                )},${colorMap(gamma, -90, 90)})`;
+
+                dotRef.current.style.borderRadius = `${rangeMap(
+                  alpha,
+                  0,
+                  360,
+                  0,
+                  50
+                )}%`;
+
+                onChange({ alpha, beta, gamma });
+              }
+            });
+          })
+          .catch(() => {
+            setHasMotionDevice(false);
+          });
+      }
+    });
+  };
+
+  return (
+    <>
+      <Card>
+        {!hasMotionDevice && (
+          <CardContent>
+            <Button
+              onClick={motionAccess}
+              variant="outlined"
+              color="primary"
+              fullWidth
+            >
+              No motion device detected. Click to request access
+            </Button>
+
+            {error && (
+              <Box>
+                <Error color="error" />
+                <Typography gutterBottom variant="caption">
+                  {error}
+                </Typography>
+              </Box>
+            )}
+          </CardContent>
+        )}
+        <div
+          ref={boardRef}
+          style={{
+            width: "100%",
+            height: "300px",
+            position: "relative",
+            overflow: "hidden",
+            margin: "0 auto",
+            backgroundColor: "#CCC",
+          }}
+        >
+          <div
+            ref={dotRef}
+            style={{
+              width: DOT_SIZE,
+              height: DOT_SIZE,
+              backgroundColor: "rgba(0, 0, 0 ,0)",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              borderRadius: "50%",
+              transition: "all 0.04s",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              backgroundColor: "rgba(0,0,0,0)",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </div>
+      </Card>
+    </>
+  );
+};
--- client/src/FmSynth.tsx
+++ client/src/FmSynth.tsx
@@ -7,14 +7,28 @@ import {
   CardContent,
   Slider,
 } from "@material-ui/core";
-import { Waves } from "@material-ui/icons";
+import { VolumeUp, Waves } from "@material-ui/icons";
+import { AccelPad, AccelParams } from "./AccelPad";
+import { rangeMap } from "./helpers";
+
+const MAX_GAIN = 10000;
+const MAX_FREQUENCY = 1000;
 
 export const FmSynth: React.FC<RouteComponentProps> = () => {
   const [mod, setMod] = React.useState<OscillatorNode | null>(null);
+  const [gain, setGain] = React.useState<GainNode | null>(null);
   const [freq, setFreq] = React.useState(440);
+  const [gainVal, setGainVal] = React.useState(3000);
+  const [text, setText] = React.useState("Not running");
 
-  const playNote = () => {
+  const startSynth = () => {
+    // Create audio context
+    const AudioContext =
+      window.AudioContext || (window as any).webkitAudioContext;
     const ctx = new AudioContext();
+    ctx.resume();
+
+    // Setup
     const out = ctx.destination;
 
     // Instantiating
@@ -27,7 +41,7 @@ export const FmSynth: React.FC<RouteComponentProps> = () => {
 
     // Modulation depth
     const E_gain = ctx.createGain();
-    E_gain.gain.value = 3000;
+    E_gain.gain.value = gainVal;
 
     // Wiring everything up
     E.connect(E_gain);
@@ -38,13 +52,34 @@ export const FmSynth: React.FC<RouteComponentProps> = () => {
     E.start();
     F.start();
 
+    setText(JSON.stringify(E.start));
+
     setMod(E);
+    setGain(E_gain);
   };
 
   const handleFreqChange = (_event: unknown, value: number | number[]) => {
     if (mod && typeof value === "number") {
       mod.frequency.value = value;
-      setFreq(value);
+      setFreq(mod.frequency.value);
+    }
+  };
+
+  const handleGainChange = (_event: unknown, value: number | number[]) => {
+    if (gain && typeof value === "number") {
+      gain.gain.value = value;
+      setGainVal(gain.gain.value);
+    }
+  };
+
+  const handleAccel = (value: AccelParams) => {
+    if (mod) {
+      mod.frequency.value = rangeMap(value.gamma, -90, 90, 0, MAX_FREQUENCY);
+      setFreq(mod.frequency.value);
+    }
+    if (gain) {
+      gain.gain.value = rangeMap(value.beta, -90, 90, 0, MAX_GAIN);
+      setGainVal(gain.gain.value);
     }
   };
 
@@ -52,9 +87,10 @@ export const FmSynth: React.FC<RouteComponentProps> = () => {
     <>
       <Card>
         <CardContent>
+          {text}
           {!mod && (
             <Button
-              onClick={playNote}
+              onClick={startSynth}
               variant="outlined"
               color="primary"
               fullWidth
@@ -70,14 +106,27 @@ export const FmSynth: React.FC<RouteComponentProps> = () => {
               <Slider
                 value={freq}
                 onChange={handleFreqChange}
-                max={1000}
-                min={10}
+                max={MAX_FREQUENCY}
+                min={0}
+                step={1}
+              />
+            </>
+          )}
+          {gain && (
+            <>
+              <VolumeUp />
+              <Slider
+                value={gainVal}
+                onChange={handleGainChange}
+                max={MAX_GAIN}
+                min={0}
                 step={1}
               />
             </>
           )}
         </CardActions>
       </Card>
+      <AccelPad onChange={handleAccel} />
     </>
   );
 };
